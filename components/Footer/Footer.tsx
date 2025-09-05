6967fd8bdcb2dfda42fa309b28035826f37e9250
Nataliia <natashazaremba82@gmail.com>
2025-09-05 20:20:29 +0300

    change

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,12 +3,12 @@ const Footer = () => {
   return (
     <footer className={css.footer}>
       <div className={css.content}>
-        <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} NoteHub</p>
         <div className={css.wrap}>
-          <p>Developer: Alina Furda</p>
+          <p>Developer: student GoIT </p>
           <p>
             Contact us:
-            <a href="mailto:student@notehub.app">alina.furda1806@gmail.com</a>
+            <a href="mailto:student@notehub.app">student@notehub.app</a>
           </p>
         </div>
       </div>
